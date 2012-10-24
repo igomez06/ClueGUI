c5b9c0c82dcb72cafb1802c5d6e79153b6764680
Lars Walen <lars@walen.me>
2012-10-24 17:54:19 -0600

    added players classes

---
+++ src/clue/ComputerPlayer.java
@@ -0,0 +1,16 @@
+package clue;
+
+import java.util.HashSet;
+
+public class ComputerPlayer extends Player {
+	private char lastRoomVisited;
+	public void pickLocation(HashSet<BoardCell> targets) {
+		
+	}
+	public void createSuggestion() {
+		
+	}
+	public void updateSeen(Card seen) {
+		
+	}
+}
---
+++ src/clue/HumanPlayer.java
@@ -0,0 +1,5 @@
+package clue;
+
+public class HumanPlayer {
+
+}
