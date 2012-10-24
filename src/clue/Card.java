740305285df2dd20dde17c88b10678555e2ab18f
Lars Walen <lars@walen.me>
2012-10-24 17:45:20 -0600

    created Card class

---
+++ src/clue/Card.java
@@ -0,0 +1,43 @@
+package clue;
+
+public class Card {
+	private String name;
+	private CardType type;
+
+	public enum CardType { ROOM, WEAPON, PERSON}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Card other = (Card) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (type != other.type)
+			return false;
+		return true;
+	}
+
+	public CardType getType() {
+		return type;
+	}
+
+	public void setType(CardType type) {
+		this.type = type;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	};
+}
