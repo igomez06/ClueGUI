f34db150d7cf84910e5c700278e8b30cf4fb3901
Lars Walen <lars@walen.me>
2012-10-25 09:00:20 -0600

    Added testing classes

---
+++ src/Tests/GameActionsTests.java
@@ -0,0 +1,33 @@
+package Tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GameActionsTests {
+
+	@BeforeClass
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testCheckAccusation() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testStartingLocation() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDisproveSuggestion() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testMakeSuggestion() {
+		fail("Not yet implemented");
+	}
+}
---
+++ src/Tests/GameSetupTest.java
@@ -0,0 +1,28 @@
+package Tests;
+
+import static org.junit.Assert.fail;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GameSetupTest {
+
+	@BeforeClass
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testLoadingPlayers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoadingCards() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testDealingCards() {
+		fail("Not yet implemented");
+	}
+}
