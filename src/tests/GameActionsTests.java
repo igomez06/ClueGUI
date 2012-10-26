4c05c83f5c07ed5bafc0603824ea5ab644e76187
Craig <craigsidcarlson@gmail.com>
2012-10-25 22:53:26 -0600

    random cell test

--- src/tests/GameActionsTests.java
+++ src/tests/GameActionsTests.java
@@ -59,13 +59,41 @@ public class GameActionsTests {
 		targets.add(rc);
 		targets.add(new WalkwayCell());
 		targets.add(new WalkwayCell());
+		targets.add(new WalkwayCell());
 
 		for (int i = 0; i < targets.size(); i++) {
 			Assert.assertEquals(cp.pickLocation(targets), rc);
 		}
-
+		HashSet<BoardCell> randomTargets = new HashSet<BoardCell>();
+		WalkwayCell wc1 = new WalkwayCell();
+		WalkwayCell wc2 = new WalkwayCell();
+		WalkwayCell wc3 = new WalkwayCell();
+		WalkwayCell wc4 = new WalkwayCell();
+		randomTargets.add(wc1);
+		randomTargets.add(wc2);
+		randomTargets.add(wc3);
+		randomTargets.add(wc4);
 		//Random choice test
-
+		int wc1Counter = 0;
+		int wc2Counter = 0;
+		int wc3Counter = 0;
+		int wc4Counter = 0;
+		for (int i = 0; i < 100; i++) {
+			if(cp.pickLocation(randomTargets).equals(wc1)) {
+				wc1Counter++;
+			}else if(cp.pickLocation(randomTargets).equals(wc2)) {
+				wc2Counter++;
+			}else if(cp.pickLocation(randomTargets).equals(wc3)) {
+				wc3Counter++;
+			}else if(cp.pickLocation(randomTargets).equals(wc4)) {
+				wc4Counter++;
+			}
+		}
+		
+		Assert.assertTrue(wc1Counter > 1 && wc1Counter < 100);
+		Assert.assertTrue(wc2Counter > 1 && wc2Counter < 100);
+		Assert.assertTrue(wc3Counter > 1 && wc3Counter < 100);
+		Assert.assertTrue(wc4Counter > 1 && wc4Counter < 100);
 
 	}
 
